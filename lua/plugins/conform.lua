1beba565268e0990531576f96b970a5b41f3c280
Reilly Spitzfaden <reillypascal@gmail.com>
2025-11-20 09:30:10 -0500

    conform and treesitter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -33,6 +33,8 @@ return { -- Autoformat
 			--
 			-- You can use 'stop_after_first' to run the first available formatter from the list
 			-- javascript = { "prettierd", "prettier", stop_after_first = true },
+			c = { "clang-format" },
+			cpp = { "clang-format" },
 			javascript = { "biome" },
 			lua = { "stylua" },
 			markdown = { "prettierd" },
