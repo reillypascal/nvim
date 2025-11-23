b4115f3e08aebcfcb75cbfe6c0bba4b3aa75ab56
Reilly Spitzfaden <reillypascal@gmail.com>
2025-11-22 21:54:03 -0500

    perf.nvim from remote; update Lazy

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,7 @@
 return {
 	-- "reillypascal/perf",
-	dir = "~/Dev/Neovim/perf.nvim",
+	"reillypascal/perf.nvim",
+	-- dir = "~/Dev/Neovim/perf.nvim",
 	lazy = false,
 	priority = 1000, -- Make sure to load this before all the other start plugins.
 	config = function()
