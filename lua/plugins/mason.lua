f0d3910458c0069aab08ef3c67da2595f7f1d652
Reilly Spitzfaden <reillypascal@gmail.com>
2025-12-01 02:33:38 -0500

    spelling, lazy updates, mason install list

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,8 +2,8 @@ return {
 	"mason-org/mason.nvim",
 	opts = {
 		ensure_installed = {
-			"basedpyright",
-			"bash-language-server",
+			-- "basedpyright",
+			-- "bash-language-server",
 			-- "biome", -- web/JS linting/formatting
 			-- "clang-format",
 			-- "clangd",
@@ -18,22 +18,22 @@ return {
 			-- 'js-debug-adapter',
 			"json-lsp",
 			-- "ltex-ls-plus",
-			"lua-language-server",
+			-- "lua-language-server",
 			-- "marksman",
 			-- "ormolu",
 			-- "prettierd",
 			-- 'pyrefly', -- python; basedpyright replacement? in rust
-			"ruff",
+			-- "ruff",
 			-- DON'T install rust-analyzer via Mason; ends up running twice
 			-- "rust_analyzer",
 			-- "stylelint", -- outdated; using biome
-			"stylua", -- Used to format Lua code
+			-- "stylua", -- Used to format Lua code
 			-- "tombi", -- TOML - new, but seems nicer than taplo
 			-- 'typescript-language-server',
 			-- 'ty', -- python: replacement for basedpyright; same people as ruff; alpha but promising
 			-- people on Reddit say vtsls is better than ts_ls
 			"vtsls",
-			"yaml-language-server",
+			-- "yaml-language-server",
 		},
 	},
 	-- also see  mason-lspconfig.nvim, mason-tool-installer.nvim
