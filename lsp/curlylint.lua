4544e2183d39795dcecd7fe2f8139743f0f31dd8
Reilly Spitzfaden <reillypascal@gmail.com>
2025-12-03 17:57:16 -0500

    curlylint root files

--- lsp/curlylint.lua
+++ lsp/curlylint.lua
@@ -1,6 +1,6 @@
 return {
 	cmd = { "curlylint" },
 	filetypes = { "liquid", "nunjucks", "jinja" },
-	root_markers = { ".git" },
+	root_markers = { ".git", "pyproject.toml" },
 	workspace_required = false,
 }
