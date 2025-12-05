2616c0424a8550755f0d243db24935c5277d0900
Reilly Spitzfaden <reillypascal@gmail.com>
2025-12-05 01:08:29 -0500

    manual statusline

--- lua/config/statusline.lua
+++ lua/config/statusline.lua
@@ -1,3 +1,119 @@
+-- https://nuxsh.is-a.dev/blog/custom-nvim-statusline.html
+-- https://elianiva.my.id/posts/neovim-lua-statusline/
+
+-- local modes = {
+-- 	["n"] = "N",
+-- 	["no"] = "N•P",
+-- 	["v"] = "V",
+-- 	["V"] = "V•L",
+-- 	[""] = "V•B",
+-- 	["s"] = "S",
+-- 	["S"] = "S•L",
+-- 	[""] = "S•B",
+-- 	["i"] = "I",
+-- 	["ic"] = "I",
+-- 	["R"] = "R",
+-- 	["Rv"] = "V•R",
+-- 	["c"] = "C",
+-- 	["cv"] = "V•E",
+-- 	["ce"] = "E",
+-- 	["r"] = "P",
+-- 	["rm"] = "M",
+-- 	["r?"] = "C",
+-- 	["!"] = "S",
+-- 	["t"] = "T",
+-- }
+
+local modes = {
+	["n"] = "NORMAL",
+	["no"] = "NORMAL",
+	["v"] = "VISUAL",
+	["V"] = "VISUAL LINE",
+	[""] = "VISUAL BLOCK",
+	["s"] = "SELECT",
+	["S"] = "SELECT LINE",
+	[""] = "SELECT BLOCK",
+	["i"] = "INSERT",
+	["ic"] = "INSERT",
+	["R"] = "REPLACE",
+	["Rv"] = "VISUAL REPLACE",
+	["c"] = "COMMAND",
+	["cv"] = "VIM EX",
+	["ce"] = "EX",
+	["r"] = "PROMPT",
+	["rm"] = "MORE",
+	["r?"] = "CONFIRM",
+	["!"] = "SHELL",
+	["t"] = "TERMINAL",
+}
+
+local function mode()
+	local current_mode = vim.api.nvim_get_mode().mode
+	return string.format(" %s ", modes[current_mode]):upper()
+end
+
+local vcs = function()
+	local git_info = vim.b.gitsigns_status_dict
+	if not git_info or git_info.head == "" then
+		return ""
+	end
+	local added = git_info.added and (" %#GitSignsAdd#+" .. git_info.added) or ""
+	local changed = git_info.changed and (" %#GitSignsChange#~" .. git_info.changed) or ""
+	local removed = git_info.removed and (" %#GitSignsDelete#-" .. git_info.removed) or ""
+	if git_info.added == 0 then
+		added = ""
+	end
+	if git_info.changed == 0 then
+		changed = ""
+	end
+	if git_info.removed == 0 then
+		removed = ""
+	end
+	return table.concat({
+		" %#GitSignsAdd# ",
+		git_info.head,
+		" ",
+		added,
+		changed,
+		removed,
+		"%#StatusLine#",
+	})
+end
+
+local function lsp()
+	local count = {}
+	local levels = {
+		errors = "Error",
+		warnings = "Warn",
+		info = "Info",
+		hints = "Hint",
+	}
+
+	local errors = ""
+	local warnings = ""
+	local hints = ""
+	local info = ""
+
+	for k, level in pairs(levels) do
+		count[k] = vim.tbl_count(vim.diagnostic.get(0, { severity = level }))
+	end
+
+	if count["errors"] ~= 0 then
+		errors = " %#LspDiagnosticsSignError#󰅚 " .. count["errors"]
+	end
+	if count["warnings"] ~= 0 then
+		warnings = " %#LspDiagnosticsSignWarning#󰀪 " .. count["warnings"]
+	end
+	if count["hints"] ~= 0 then
+		hints = " %#LspDiagnosticsSignHint#󰋽 " .. count["hints"]
+	end
+	if count["info"] ~= 0 then
+		info = " %#LspDiagnosticsSignInformation#󰌶 " .. count["info"]
+	end
+
+	return " " .. errors .. warnings .. hints .. info .. " "
+end
+
 local function filepath()
 	local fpath = vim.fn.fnamemodify(vim.fn.expand("%"), ":~:.:h")
 	if fpath == "" or fpath == "." then
@@ -13,35 +129,6 @@ local function filename()
 		return ""
 	end
 	return fname .. " "
-end
-
-local vcs = function()
-	local git_info = vim.b.gitsigns_status_dict
-	if not git_info or git_info.head == "" then
-		return ""
-	end
-	local added = git_info.added and ("%#GitSignsAdd#+" .. git_info.added .. " ") or ""
-	local changed = git_info.changed and ("%#GitSignsChange#~" .. git_info.changed .. " ") or ""
-	local removed = git_info.removed and ("%#GitSignsDelete#-" .. git_info.removed .. " ") or ""
-	if git_info.added == 0 then
-		added = ""
-	end
-	if git_info.changed == 0 then
-		changed = ""
-	end
-	if git_info.removed == 0 then
-		removed = ""
-	end
-	return table.concat({
-		" ",
-		added,
-		changed,
-		removed,
-		" ",
-		"%#GitSignsAdd# ",
-		git_info.head,
-		" %#Normal#",
-	})
 end
 
 local function filetype()
@@ -61,26 +148,35 @@ Statusline.active = function()
 	return table.concat({
 		"%#Statusline#",
 		-- update_mode_colors(),
-		-- mode(),
-		"%#Normal#",
+		mode(),
+		"%#StatusLine#",
+		vcs(),
+		"%#StatusLine#",
+		lsp(),
+		"%#StatusLine#",
 		filepath(),
 		filename(),
-		"%#Normal#",
-		vcs(),
-		"%#Normal#",
-		-- lsp(),
 		"%=%#StatusLineExtra#",
 		filetype(),
 		lineinfo(),
 	})
 end
+
+function Statusline.inactive()
+	return " %F"
+end
+
+function Statusline.short()
+	return "%#StatusLineNC#   NvimTree"
+end
+
 vim.cmd(
 	[[
 augroup Statusline
 	au!
 	au WinEnter,BufEnter * setlocal statusline=%!v:lua.Statusline.active()
-	" au WinLeave,BufLeave * setlocal statusline=%!v:lua.Statusline.inactive()
-	" au WinEnter,BufEnter,FileType NvimTree setlocal statusline=%!v:lua.Statusline.short()
+	au WinLeave,BufLeave * setlocal statusline=%!v:lua.Statusline.inactive()
+	au WinEnter,BufEnter,FileType NvimTree setlocal statusline=%!v:lua.Statusline.short()
 	augroup END
 	]],
 	false
