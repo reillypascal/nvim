79da870cc53d02c2aa9e215a9c25d9722cf6f868
Reilly Spitzfaden <reillypascal@gmail.com>
2025-12-18 19:35:13 -0500

    using remote for color scheme

--- lua/plugins/radio.lua
+++ lua/plugins/radio.lua
@@ -1,6 +1,6 @@
 return {
-	-- "reillypascal/radio.nvim",
-	dir = "~/Dev/Neovim/radio.nvim",
+	"reillypascal/radio.nvim",
+	-- dir = "~/Dev/Neovim/radio.nvim",
 	lazy = false,
 	priority = 1000, -- Make sure to load this before all the other start plugins.
 	config = function()
