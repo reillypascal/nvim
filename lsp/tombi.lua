b65a468e5088bacacca85a3d443d2258cc85b167
Reilly Spitzfaden <reillypascal@gmail.com>
2025-12-31 18:20:45 -0500

    formatting, lazy updates

--- lsp/tombi.lua
+++ lsp/tombi.lua
@@ -7,7 +7,7 @@
 
 ---@type vim.lsp.Config
 return {
-  cmd = { 'tombi', 'lsp' },
-  filetypes = { 'toml' },
-  root_markers = { 'tombi.toml', 'pyproject.toml', '.git' },
+	cmd = { "tombi", "lsp" },
+	filetypes = { "toml" },
+	root_markers = { "tombi.toml", "pyproject.toml", ".git" },
 }
