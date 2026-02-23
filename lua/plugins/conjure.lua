b1626cb352745e62a03099ea9a4ff062d6e6dd64
Reilly Spitzfaden <reillypascal@gmail.com>
2026-02-23 18:23:12 -0500

    testing guile repl for conjure

--- lua/plugins/conjure.lua
+++ lua/plugins/conjure.lua
@@ -1,1 +1,9 @@
-return { "Olical/conjure" }
+return {
+	"Olical/conjure",
+	-- config = function()
+	-- 	vim.cmd([[
+	-- 		let g:conjure#filetype#scheme = "conjure.client.guile.socket"
+	-- 		let g:conjure#client#guile#socket#pipename = getcwd() + "/.guile-repl.socket"
+	-- 	]])
+	-- end,
+}
