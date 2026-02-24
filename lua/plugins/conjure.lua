306022801873b088701d1bd4f3f6aa31158c36a7
Reilly Spitzfaden <reillypascal@gmail.com>
2026-02-24 00:50:30 -0500

    use guile socket in conjure

--- lua/plugins/conjure.lua
+++ lua/plugins/conjure.lua
@@ -1,9 +1,9 @@
 return {
 	"Olical/conjure",
-	-- config = function()
-	-- 	vim.cmd([[
-	-- 		let g:conjure#filetype#scheme = "conjure.client.guile.socket"
-	-- 		let g:conjure#client#guile#socket#pipename = getcwd() + "/.guile-repl.socket"
-	-- 	]])
-	-- end,
+	config = function()
+		vim.cmd([[
+			let g:conjure#filetype#scheme = "conjure.client.guile.socket"
+			let g:conjure#client#guile#socket#pipename = ".guile-repl.socket"
+		]])
+	end,
 }
