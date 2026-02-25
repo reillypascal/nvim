e283b6f71b002f7500f73835fb4c756ff539accf
Reilly Spitzfaden <reillypascal@gmail.com>
2026-02-25 02:19:01 -0500

    vim -> lua in conjure; line wrap on; no HUD

--- lua/plugins/conjure.lua
+++ lua/plugins/conjure.lua
@@ -1,13 +1,17 @@
 return {
 	"Olical/conjure",
 	config = function()
-		vim.cmd([[
-			" Guile: https://github.com/Olical/conjure/wiki/Quick-start:-Guile-(socket)
-			" let g:conjure#filetype#scheme = "conjure.client.guile.socket"
-			" let g:conjure#client#guile#socket#pipename = ".guile-repl.socket"
-			" Lilypond Scheme Sandbox: https://github.com/Olical/conjure/wiki/Quick-start:-Scheme-(stdio)
-			let g:conjure#client#scheme#stdio#command = "lilypond scheme-sandbox"
-			let g:conjure#client#scheme#stdio#prompt_pattern = "scheme@(.*)> "
-		]])
+		-- default: { "clojure", "elixir", "fennel", "janet", "hy", "julia", "racket", "scheme", "lua", "lisp", "python", "ruby", "rust", "sql" }
+		vim.g["conjure#filetypes"] = { "lisp", "scheme" }
+		vim.g["conjure#log#wrap"] = true
+		vim.g["conjure#log#hud#enabled"] = false
+		-- guile
+		-- https://github.com/Olical/conjure/wiki/Quick-start:-Guile-(socket)
+		-- vim.g["conjure#filetype#scheme"] = "conjure.client.guile.socket"
+		-- vim.g["conjure#client#guile#socket#pipename"] = ".guile-repl.socket"
+		-- lilypond scheme sandbox
+		-- https://github.com/Olical/conjure/wiki/Quick-start:-Scheme-(stdio)
+		vim.g["conjure#client#scheme#stdio#command"] = "lilypond scheme-sandbox"
+		vim.g["conjure#client#scheme#stdio#prompt_pattern"] = "scheme@(.*)> "
 	end,
 }
