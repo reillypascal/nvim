d12838e33309dfaad36f20cdee02de3f196dcbcd
Reilly Spitzfaden <reillypascal@gmail.com>
2026-02-24 19:33:44 -0500

    lilypond sandbox repl for conjure

--- lua/plugins/conjure.lua
+++ lua/plugins/conjure.lua
@@ -2,8 +2,12 @@ return {
 	"Olical/conjure",
 	config = function()
 		vim.cmd([[
-			let g:conjure#filetype#scheme = "conjure.client.guile.socket"
-			let g:conjure#client#guile#socket#pipename = ".guile-repl.socket"
+			" Guile: https://github.com/Olical/conjure/wiki/Quick-start:-Guile-(socket)
+			" let g:conjure#filetype#scheme = "conjure.client.guile.socket"
+			" let g:conjure#client#guile#socket#pipename = ".guile-repl.socket"
+			" Lilypond Scheme Sandbox: https://github.com/Olical/conjure/wiki/Quick-start:-Scheme-(stdio)
+			let g:conjure#client#scheme#stdio#command = "lilypond scheme-sandbox"
+			let g:conjure#client#scheme#stdio#prompt_pattern = "> "
 		]])
 	end,
 }
