e0e7e6dbf75a373b3e1189cd858475dabec48fb1
Reilly Spitzfaden <reillypascal@gmail.com>
2026-03-11 09:16:52 -0400

    revert scnvim keymaps

--- lua/plugins/scnvim.lua
+++ lua/plugins/scnvim.lua
@@ -1,6 +1,7 @@
 return {
 	"davidgranstrom/scnvim",
 	ft = "supercollider",
+	lazy = true,
 	dependencies = { "davidgranstrom/scnvim-tmux" },
 	config = function()
 		local scnvim = require("scnvim")
@@ -9,22 +10,23 @@ return {
 
 		scnvim.setup({
 			keymaps = {
-				["<localleader>ee"] = map("editor.send_line", { "i", "n" }, { buffer = true }),
-				["<localleader>er"] = {
+				["<S-CR>"] = map("editor.send_line", { "i", "n" }, { buffer = true }),
+				["<C-CR>"] = {
 					map("editor.send_block", { "i", "n" }, { buffer = true }),
 					map("editor.send_selection", "x", { buffer = true }),
 				},
-				["<localleader>spt"] = map("postwin.toggle", { buffer = true }),
-				["<localleader>spc"] = map("postwin.clear", { "n", "i" }, { buffer = true }),
-				["<localleader>si"] = map("signature.show", { "n", "i" }, { buffer = true }),
-				["<localleader>s."] = map("sclang.hard_stop", { "n", "x", "i" }, { buffer = true }),
-				["<localleader>ss"] = map("sclang.start", { "n", "x", "i" }, { buffer = true }),
-				["<localleader>sr"] = map("sclang.recompile", { "n", "x", "i" }, { buffer = true }),
-				["<localleader>sb"] = map_expr("s.boot", { "n", "x", "i" }, { buffer = true }),
-				["<localleader>sk"] = map_expr("Server.killAll", { "n", "x", "i" }, { buffer = true }),
-				["<localleader>sq"] = map_expr("s.quit", { "n", "x", "i" }, { buffer = true }),
-				["<localleader>sm"] = map_expr("s.meter", { "n", "x", "i" }, { buffer = true }),
-				["<localleader>so"] = map_expr("s.scope", { "n", "x", "i" }, { buffer = true }),
+				["<CR>"] = map("postwin.toggle", { buffer = true }),
+				["<M-CR>"] = map("postwin.toggle", "i", { buffer = true }),
+				["<M-L>"] = map("postwin.clear", { "n", "i" }, { buffer = true }),
+				["<C-k>"] = map("signature.show", { "n", "i" }, { buffer = true }),
+				["<localleader>c."] = map("sclang.hard_stop", { "n", "x", "i" }, { buffer = true }),
+				["<localleader>cs"] = map("sclang.start", { "n", "x", "i" }, { buffer = true }),
+				["<localleader>cr"] = map("sclang.recompile", { "n", "x", "i" }, { buffer = true }),
+				["<localleader>cb"] = map_expr("s.boot", { "n", "x", "i" }, { buffer = true }),
+				["<localleader>ck"] = map_expr("Server.killAll", { "n", "x", "i" }, { buffer = true }),
+				["<localleader>cq"] = map_expr("s.quit", { "n", "x", "i" }, { buffer = true }),
+				["<localleader>cm"] = map_expr("s.meter", { "n", "x", "i" }, { buffer = true }),
+				["<localleader>co"] = map_expr("s.scope", { "n", "x", "i" }, { buffer = true }),
 			},
 			editor = {
 				highlight = {
@@ -37,7 +39,7 @@ return {
 			-- },
 			postwin = {
 				float = {
-					enabled = true,
+					enabled = false,
 				},
 			},
 			extensions = {
