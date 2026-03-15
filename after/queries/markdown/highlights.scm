4bbddbfe8a8b674bef2a50889ec86e495731942c
Reilly Spitzfaden <reillypascal@gmail.com>
2026-03-15 12:18:48 -0400

    markdown spell treesitter queries

--- after/queries/markdown/highlights.scm
+++ after/queries/markdown/highlights.scm
@@ -1,5 +1,8 @@
 ;; extends
-((inline)
-      @inline (#contains? @inline "https://")) @nospell
-((inline)
-      @inline (#contains? @inline "http://")) @nospell
+((link_destination)
+      @link_destination (#match? @link_destination "\\w+:\\/\\/[^[:space:]]+")) @nospell
+; ((inline)
+;       @element (#match? @element "<\\w+>")) @nospell
+; https://davisvaughan.github.io/r-tree-sitter/reference/query-matches-and-captures.html#-match-regex
+; ((inline)
+;       @inline (#contains? @inline "http://")) @nospell
