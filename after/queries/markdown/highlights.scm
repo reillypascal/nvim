11b49cfc03069094c8b35e242ca0363d3ac7246d
Reilly Spitzfaden <reillypascal@gmail.com>
2026-03-17 09:29:34 -0400

    added sqls, updated lazy/spell

--- after/queries/markdown/highlights.scm
+++ after/queries/markdown/highlights.scm
@@ -1,8 +1,13 @@
 ;; extends
-((link_destination)
-      @link_destination (#match? @link_destination "\\w+:\\/\\/[^[:space:]]+")) @nospell
+
+; https://vi.stackexchange.com/questions/44708/neovim-override-treesitter-spelling-settings
+; https://davisvaughan.github.io/r-tree-sitter/reference/query-matches-and-captures.html#-match-regex
+(link_destination) @nospell
+; ((link_destination)
+;       @link_destination (#match? @link_destination "\\w+:\\/\\/[^[:space:]]+")) @nospell
+; ((uri_autolink)
+;       @uri_autolink (#match? @uri_autolink "\\w+:\\/\\/[^[:space:]]+")) @nospell
 ; ((inline)
 ;       @element (#match? @element "<\\w+>")) @nospell
-; https://davisvaughan.github.io/r-tree-sitter/reference/query-matches-and-captures.html#-match-regex
 ; ((inline)
 ;       @inline (#contains? @inline "http://")) @nospell
