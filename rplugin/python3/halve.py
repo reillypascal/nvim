1e577315cf6a353bf1597d915836b6405f4f3c09
Reilly Spitzfaden <reillypascal@gmail.com>
2026-03-22 10:26:37 -0400

    half > halve

--- rplugin/python3/halve.py
+++ rplugin/python3/halve.py
@@ -5,10 +5,10 @@ import lib.lyutils as lyutils
 
 
 @pynvim.plugin
-class Half(object):
+class Halve(object):
     def __init__(self, nvim):
         self.nvim = nvim
 
-    @pynvim.command("Half", range="", nargs="*", sync=True)
+    @pynvim.command("Halve", range="", nargs="*", sync=True)
     def command_handler(self, args, rng):
         lyutils.apply_rhythm(self.nvim, args, rhythm.rhythm_halve)
