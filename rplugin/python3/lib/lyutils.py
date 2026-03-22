a19de09c7a10af12227c31e336832ac640ea395c
Reilly Spitzfaden <reillypascal@gmail.com>
2026-03-22 10:23:30 -0400

    moved rhythm rplugins' body to lib file

---
+++ rplugin/python3/lib/lyutils.py
@@ -0,0 +1,31 @@
+import pynvim
+import ly.document as document
+
+
+def apply_rhythm(nvim, args, handler):
+    # get entire current buffer
+    buffer = nvim.current.buffer
+    # get visual boundary marks
+    vstart = buffer.mark("<")
+    vend = buffer.mark(">")
+
+    # index/slice portion of buffer
+    string = buffer[vstart[0] - 1][vstart[1] : vend[1] + 1]
+    # if selection doesn't include '{}',
+    # `ly` requires them, so wrap around string
+    if "{" not in string and "}" not in string:
+        string = "{" + string + "}"
+
+    # turn string into Document
+    doc = document.Document(string)
+    # perform transposition
+    if len(args) > 0:
+        handler(document.Cursor(doc, 0, None), args)
+    else:
+        handler(document.Cursor(doc, 0, None))
+
+    # result as string; remove '{}'/space
+    output = doc.plaintext().replace("{", "").replace("}", "").strip(" ")
+    # put output string in '*' register
+    nvim.funcs.setreg("*", output)
+    nvim.api.feedkeys('gv"*p', "n", False)
