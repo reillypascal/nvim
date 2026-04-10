d4f31ffcff6a5ccf9c57688907be46753edaf6f3
Reilly Spitzfaden <reillypascal@gmail.com>
2026-04-10 00:07:53 -0400

    sql tree-sitter

--- lua/plugins/tree-sitter-manager.lua
+++ lua/plugins/tree-sitter-manager.lua
@@ -3,6 +3,41 @@ return {
 	dependencies = {}, -- tree-sitter CLI must be installed system-wide
 	config = function()
 		require("tree-sitter-manager").setup({
+			-- list of parsers to install automatically
+			ensure_installed = {
+				"bash",
+				"c",
+				"cmake",
+				"cpp",
+				"css",
+				"haskell",
+				"html",
+				"javascript",
+				"json",
+				"latex",
+				"liquid",
+				"lua",
+				"make",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"rust",
+				"scheme",
+				"sql",
+				"supercollider",
+				"toml",
+				"xml",
+				"yaml",
+				"zsh",
+			},
+			languages = {
+				genexpr = {
+					install_info = {
+						url = "https://github.com/isabelgk/tree-sitter-genexpr",
+						use_repo_queries = true, -- copy queries/ from the cloned repo
+					},
+				},
+			},
 			-- Optional: custom paths
 			-- parser_dir = vim.fn.stdpath("data") .. "/site/parser",
 			-- query_dir = vim.fn.stdpath("data") .. "/site/queries",
