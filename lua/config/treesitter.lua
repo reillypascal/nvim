a077611c880042f2d41a25b1563f618948b0d09d
Reilly Spitzfaden <reillypascal@gmail.com>
2026-04-10 22:10:18 -0400

    removed ts manager

--- lua/config/treesitter.lua
+++
@@ -1,9 +0,0 @@
--- iterate through list of installed_ft
--- or pass installed_ft as list
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = installed_ft,
-	callback = function()
-		pcall(vim.treesitter.start)
-	end,
-	desc = "Auto-enable treesitter for installed parsers",
-})
