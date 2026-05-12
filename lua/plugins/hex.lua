1d7fd2ac8f6996636e23513569a5f32a33b12f3f
Reilly Spitzfaden <reillypascal@gmail.com>
2026-05-12 19:21:57 -0400

    hex.nvim only runs on dump/assemble/toggle commands; was opening e.g., .zsh_history as hex

--- lua/plugins/hex.lua
+++ lua/plugins/hex.lua
@@ -1,5 +1,31 @@
 return {
 	"RaafatTurki/hex.nvim",
+	keys = {
+		{
+			"<leader>ha",
+			function()
+				require("hex").assemble()
+			end,
+			mode = "",
+			desc = "[H]ex [A]ssemble",
+		},
+		{
+			"<leader>hd",
+			function()
+				require("hex").dump()
+			end,
+			mode = "",
+			desc = "[H]ex [D]ump",
+		},
+		{
+			"<leader>ht",
+			function()
+				require("hex").toggle()
+			end,
+			mode = "",
+			desc = "[H]ex [T]oggle",
+		},
+	},
 	config = function()
 		require("hex").setup()
 	end,
